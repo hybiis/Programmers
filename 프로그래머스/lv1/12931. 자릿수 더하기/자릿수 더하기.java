27a98d53cb8e6d6a42309c8a4a581297bfc74833
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-02-11 13:14:06 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.09 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String strn=Integer.toString(n);
+        String [] arr=strn.split("");
+        
+        for(int i=0;i<arr.length;i++){
+            answer+=Integer.parseInt(arr[i]);
+        }
+        
+        return answer;
+    }
+}
