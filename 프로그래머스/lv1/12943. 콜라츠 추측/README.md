a48860751da3774575167efc31b7aa4501944b77
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-02-11 22:20:26 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.08 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/README.md
+++ 프로그래머스/lv1/12943. 콜라츠 추측/README.md
@@ -1,10 +1,10 @@
 # [level 1] 콜라츠 추측 - 12943 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12943#) 
 
 ### 성능 요약
 
-메모리: 69.1 MB, 시간: 0.05 ms
+메모리: 76.3 MB, 시간: 0.08 ms
 
 ### 구분
 
--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,23 +1,12 @@
 class Solution {
     public int solution(int num) {
-        int count = 0;
-        long num2 = Long.valueOf(num);
-       if(num==1){
-           return 0;
-       }  
-        
-       while(num2>1){
-         if(num2%2==0){
-            num2=num2/2;
-            count++;
-        }else{
-            num2=num2*3+1;
-             count++;
-            }                   
-       }  
-        if(count>=500){
-            return -1;
+        long n= Long.valueOf(num);
+        for(int i=0;i<500;i++){
+            if(n==1) {
+                return i;
+            }
+            n=(n%2==0)?n/2:n*3+1;
         }
-       return count;
+        return -1;
     }
 }
