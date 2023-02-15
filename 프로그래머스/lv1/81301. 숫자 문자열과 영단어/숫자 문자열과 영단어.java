906ee9464e5010ef0b7e37f1162686871ee1aa42
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-02-15 16:16:43 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.29 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,26 +1,12 @@
-import java.util.*;
 class Solution {
     public int solution(String s) {
-        String answer = "";
-        String num []={"zero", "one", "two", "three" ,"four", "five" ,"six", "seven", "eight", "nine"};
-      String[] str=s.split("");
-      String n="";
+        String A []={"zero", "one", "two", "three" ,"four", "five" ,"six", "seven", "eight", "nine"};
+         String num []={"0", "1", "2", "3" ,"4", "5" ,"6", "7", "8", "9"};
         
-      for(int i=0;i<s.length();i++){
-          n+=str[i];
-          char ch=n.charAt(0);
-          for(int j=0;j<num.length;j++){
-            if(n.equals(num[j])  ){
-              n=Integer.toString(j);
-              answer+=n;
-              n="";
-            }else if(ch > 47 && ch<=57){
-              answer+=s.charAt(i);
-              n="";
-              break;
-            }
-          }
-      }
-        return Integer.parseInt(answer);
+        for(int i=0;i<10;i++){
+            s=s.replaceAll(A[i],num[i]);
+        }
+        
+        return Integer.parseInt(s);
     }
 }
