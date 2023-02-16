740408bd375657aeae66a0d43fa5333f3759d9e4
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-02-16 22:28:50 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/README.md
+++ 프로그래머스/unrated/132267. 콜라 문제/README.md
@@ -1,10 +1,10 @@
 # [unrated] 콜라 문제 - 132267 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267#) 
 
 ### 성능 요약
 
-메모리: 77.7 MB, 시간: 0.03 ms
+메모리: 75.2 MB, 시간: 0.02 ms
 
 ### 구분
 
--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -2,19 +2,10 @@ class Solution {
     public int solution(int a, int b, int n) {
         int answer = 0;
         
-        while(n>=a)
-            if(n%a==0){
-                n=n/a*b;
-                answer+=n;
-            }else{
-                for(int i=1;i<a;i++){
-                    if((n-i)%a==0){
-                        n=(n-i)/a*b;
-                        answer+=n;
-                        n=n+i;
-                    }
-                }
-            }   
+        while(n >= a) {
+            answer += n/a*b;
+            n = n/a*b + n%a;
+        }
         
         return answer;
     }
