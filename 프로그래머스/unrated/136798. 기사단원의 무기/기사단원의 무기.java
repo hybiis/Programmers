6cfacf1904c06531e3563a12f7c061def918c35c
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-02-27 13:56:30 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.17 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int count[]=new int[number+1];
+        
+        for(int i=1;i<=number;i++){
+            for(int j=1;j<=number/i;j++){
+                count[i*j]++;
+            }
+        }
+        int answer = 0;
+        
+        for(int i=1;i<count.length;i++){
+            if(count[i]>limit){
+                count[i]=power;
+                answer+=count[i];
+            }else{
+                answer+=count[i];
+            }
+        }
+        return answer;
+    }
+}
