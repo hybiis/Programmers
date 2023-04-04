80b7afa8e649b04c92c337f7aa0e277cfbb99eaf
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-04-04 19:39:22 +0900

    [level 1] Title: 내적, Time: 0.03 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.java
+++ 프로그래머스/lv1/70128. 내적/내적.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(int[] a, int[] b) {
         int answer = 0;
-        int arr[]=new int[a.length];
-        
         for(int i=0;i<a.length;i++){
-            arr[i]=a[i]*b[i];
-            answer+=arr[i];
-        }        
+            answer+=a[i]*b[i];
+        }
+        
         return answer;
     }
 }
