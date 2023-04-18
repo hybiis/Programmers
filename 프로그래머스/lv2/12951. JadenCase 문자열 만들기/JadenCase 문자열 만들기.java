aa815ea30d228eed76d8890c3e3aab3280649034
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-04-18 15:33:04 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.85 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,36 +1,15 @@
-import java.util.StringTokenizer;
-
 class Solution {
-
-    public static String solution(String s) {
-        StringTokenizer st = new StringTokenizer(s, " ", true);
-        StringBuilder sb = new StringBuilder();
-
-        while (st.hasMoreTokens()) {
-            String str = st.nextToken();
-            if (str.startsWith(" ")) {
-                sb.append(str);
-                continue;
-            }
-            if (isStartWithNumber(str)) {
-                sb.append(str.charAt(0));
-            } else if (isStartWithLowerAlpha(str)) {
-                sb.append((char)(str.charAt(0) - 32));
-            } else{
-                sb.append(str.charAt(0));
-            }
-            sb.append(str.substring(1).toLowerCase());
+    public String solution(String s) {
+        String answer = "";
+        boolean A=true;
+        
+        s=s.toLowerCase();
+        String arr[]=s.split("");
+        
+        for(String ss:arr){
+            answer+= A?ss.toUpperCase():ss;
+            A=ss.equals(" ")?true:false;
         }
-
-        return sb.toString();
+        return answer;
     }
-
-    private static boolean isStartWithLowerAlpha(String str) {
-        return str.charAt(0) >= 'a' && str.charAt(0) <= 'z';
-    }
-
-    private static boolean isStartWithNumber(String str) {
-        return str.charAt(0) >= '0' && str.charAt(0) <= '9';
-    }
-
 }
