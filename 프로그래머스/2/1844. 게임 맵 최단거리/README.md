0df998ca459743fe14638a8db2c22f036c5ca7ea
hj_00 <76191250+hybiis@users.noreply.github.com>
2023-11-29 16:26:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.57 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844#) 
 
 ### 성능 요약
 
-메모리: 52.3 MB, 시간: 4.09 ms
+메모리: 54 MB, 시간: 7.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 4일 11:18:26
+2023년 11월 3일 16:26:43
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,35 +1,36 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
-        int answer = 0;
-        int rows = maps.length;
-        int cols = maps[0].length;
+        int answer = -1;
+        int n = maps.length;
+        int m = maps[0].length;
+        int dis[][] = {{0,1},{0,-1},{1,0},{-1,0}};
+        Queue<int[]> que = new LinkedList<>();
         
-        Queue <int[]> que = new LinkedList<>();
-        que.offer(new int[]{0,0,1});
-        
-        int distances [][] = {{-1,0},{1,0},{0,1},{0,-1}};
+        que.add(new int[]{0,0,1});
         
         while(!que.isEmpty()){
-            int[] current = que.poll();
-            int row = current[0];
-            int col = current[1];
-            int distance = current[2];
+            int[] arr = que.poll();
+            int row = arr[0];
+            int col = arr[1];
+            int distance = arr[2];
             
-            if(rows-1 == row && cols-1 == col){
+            if(row == n-1 && col == m-1){
                 return distance;
             }
             
-            for(int[] dis:distances){
-                int newrow = row+dis[0];
-                int newcol = col+dis[1];
+            for(int i=0;i<4;i++){
+                int newRow = row + dis[i][0];
+                int newCol = col + dis[i][1];
                 
-                if(newrow>=0 && newrow<rows && newcol>=0 && newcol<cols && maps[newrow][newcol]==1){
-                    maps[newrow][newcol]=0;
-                    que.offer(new int[]{newrow,newcol,distance+1});
+                if(newRow>=0 && newCol>=0 && newRow<n && newCol<m && maps[newRow][newCol] == 1){
+                    maps[newRow][newCol] = 0;
+                    que.add(new int[]{newRow,newCol,distance+1});
                 }
             }
+        
         }
-        return -1;
+        
+        return answer;
     }
 }
