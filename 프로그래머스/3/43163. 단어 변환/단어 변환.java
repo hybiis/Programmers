1c91627bd4d5cb04ea5df11153197f5ccfc30682
hj_00 <76191250+hybiis@users.noreply.github.com>
2024-01-22 14:14:49 +0900

    [level 3] Title: 단어 변환, Time: 0.27 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,36 +1,51 @@
 import java.util.*;
 class Solution {
+    class Node{
+        String s;
+        int edge;
+        public Node(String s,int edge){
+            this.s = s;
+            this.edge = edge;
+        }
+    }
+    
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-         
-        if(Arrays.asList(words).indexOf(target)==-1){
-            return 0;
-        }
         
-        int l = begin.length();
-        for(int i=0;i<words.length;i++){
-            int cnt = 0;
-            int rcnt=0;
-            for(int j=0;j<l;j++){
-                if(begin.charAt(j)==words[i].charAt(j)){
-                    cnt++;
-                }
-                if(begin.charAt(j)==target.charAt(j)){
-                    rcnt++;
-                }
-            }
-            if(rcnt == l-1){
-                answer++;
-                break;
-            }
-            if(cnt== l-1){
-                answer++;
-                begin = words[i];
-            }
+        boolean chk[] = new boolean[words.length];
+        
+        Queue<Node> que = new LinkedList<>();
+        que.add(new Node(begin,0));
+        
+        while(!que.isEmpty()){
+            Node cur = que.poll();
             
+            if(cur.s.equals(target)){
+                answer= cur.edge;
+                return answer;
+            }            
+            for(int i=0;i<words.length;i++){
+                if(wordchk(cur.s,words[i]) && chk[i]==false){
+                    chk[i]=true;
+                    que.add(new Node(words[i],cur.edge+1));
+                }
+            }            
             
         }
         
         return answer;
+    }
+                
+    public boolean wordchk(String s1, String s2){
+        int num =0;
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                num++;
+                if(num>1){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
