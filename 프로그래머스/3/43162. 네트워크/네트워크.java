9ed38326d8ae952dbc95777602ac69f9802d8ee6
hj_00 <76191250+hybiis@users.noreply.github.com>
2024-01-22 11:50:01 +0900

    [level 3] Title: 네트워크, Time: 0.10 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,21 @@
-import java.util.*;
 class Solution {
-    boolean check[];
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        check = new boolean[n];
-        
+        boolean[] chk = new boolean[n];
         for(int i=0;i<n;i++){
-            if(!check[i]){
-                dfs(n,computers,i);
+            if(chk[i]==false){
+                dfs(i,chk,computers);
                 answer++;
-            } 
+            }
         }
-        
         return answer;
     }
-    public void dfs(int n, int[][]computers,int k){
-        check[k] = true;
-        for(int i=0;i<n;i++){
-            if(!check[i] && computers[k][i]==1){
-                dfs(n,computers,i);
+    public void dfs(int i, boolean[] chk, int[][] computers){
+        chk[i]=true;
+        
+        for(int j=0;j<computers.length;j++){
+            if(i!=j && computers[i][j]==1 && chk[j]==false){
+                dfs(j,chk,computers);
             }
         }
     }
