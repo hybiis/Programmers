bcaa773b1b255fd8a5fd4e04dac55bc0b0b53221
hj_00 <76191250+hybiis@users.noreply.github.com>
2024-01-22 10:06:58 +0900

    [level 1] Title: 폰켓몬, Time: 2.15 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,19 +1,21 @@
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
-        int answer = 0;
-        Set<Integer> set = new HashSet<>();
+        int answer = 0;       
+        int len = nums.length/2;
+        
+        Set <Integer> set = new HashSet<>();
         
         for(int i:nums){
             set.add(i);
         }
         
-        int k = nums.length/2;
-        if(k>set.size()){
-            return set.size();
+        if(set.size()<=len){
+            answer= set.size();
         }else{
-            answer = k;
+            answer = len;
         }
+        
         return answer;
     }
 }
