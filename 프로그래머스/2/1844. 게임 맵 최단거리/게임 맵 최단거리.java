6796b5e4231f00e774e1ef4e411059f5e3997b39
hj_00 <76191250+hybiis@users.noreply.github.com>
2024-01-22 13:26:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.99 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,35 +1,35 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
-        int xy [][] = {{-1,0},{1,0},{0,-1},{0,1}};
-        int start [] = {0,0};
+        int answer = -1;
+        int xy[][] = {{0,1},{0,-1},{1,0},{-1,0}};
         
-        Queue <int []> que = new LinkedList<>();
-        que.add(new int[] {0,0,1});
+        Queue <int[]> que = new LinkedList<>();
+        
+        if(que.isEmpty()){
+            que.add(new int[]{0,0,1});
+        }
         
         while(!que.isEmpty()){
-            int arr[] = que.poll();
-            int x = arr[0];
-            int y = arr[1];
-            int dis = arr[2];
+            int n[] = que.poll();
+            int x = n[0];
+            int y = n[1];
+            int distance = n[2];
             
             if(x==maps.length-1 && y==maps[0].length-1){
-                return dis;
+                return distance;
             }
             
             for(int i=0;i<4;i++){
-                int newx = x + xy[i][0];
-                int newy = y + xy[i][1];
+                int newx = x+xy[i][0];
+                int newy = y+xy[i][1];
                 
-                if(newx>=0 && newy>=0 && newx<maps.length && newy<maps[0].length && maps[newx][newy]==1){
-                    maps[newx][newy] = 0;
-                    que.add(new int[]{newx,newy,dis+1});
+                if(newx>=0 && newy>=0 && newx<maps.length && newy<maps[0].length &&maps[newx][newy]==1){
+                    maps[newx][newy]=0;
+                    que.add(new int[]{newx,newy,distance+1});
                 }
-            }
-            
+            }            
         }
-        
-        return -1;
+        return answer;
     }
-
 }
